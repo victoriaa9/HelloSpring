a90371df895238dd8d9a386bb1050ab6963d0f68
victoriaa9 <vzakharchenko99@gmail.com>
2025-10-21 19:27:23 +0200

    added first Spring Boot Project

---
+++ hello/src/main/java/edu/fra/uas/hello/controller/HelloWorldCrontroller.java
@@ -0,0 +1,16 @@
+package edu.fra.uas.hello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldCrontroller {
+     
+    @RequestMapping(value="hello", method=RequestMethod.GET)
+    @ResponseBody
+    public String getHelloWorld() {
+        return "<h1>Hello World Spring Boot</h1>!";
+    }
+}
